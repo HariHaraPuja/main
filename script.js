aff582dc7fca7340c5fdec19646da08d71108ecf
HariHaraPuja <hariharpuja.prime@gmail.com>
2024-12-26 18:01:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for contacting us!');
+});
+function redirect(){
+    location.href="booking.html";
+}
+function redirect_service1(){
+    location.href="services1.html"
+}
+function redirect_service2(){
+    location.href="services2.html"
+}
+function redirect_service3(){
+    location.href="services3.html"
+}
+function redirect_service4(){
+    location.href="services4.html"
+}
+function redirect_service5(){
+    location.href="services5.html"
+}
+function redirect_service6(){
+    location.href="services6.html"
+}
+function redirect_service7(){
+    location.href="services7.html"
+}
+function redirect_service8(){
+    location.href="services8.html"
+}
+function showCustomAlert() {
+    document.getElementById('customAlert').style.display = 'block';
+}
+function showCustomAlert1() {
+    document.getElementById('customAlert1').style.display = 'block';
+}
+function showCustomAlert2() {
+    document.getElementById('customAlert2').style.display = 'block';
+}
+function showCustomAlert3() {
+    document.getElementById('customAlert3').style.display = 'block';
+}
+function showCustomAlert4() {
+    document.getElementById('customAlert4').style.display = 'block';
+}
+function showCustomAlert5() {
+    document.getElementById('customAlert5').style.display = 'block';
+}
+function showCustomAlert6() {
+    document.getElementById('customAlert6').style.display = 'block';
+}
+function closeCustomAlert() {
+    document.getElementById('customAlert1').style.display = 'none';
+    document.getElementById('customAlert2').style.display = 'none';
+    document.getElementById('customAlert3').style.display = 'none';
+    document.getElementById('customAlert4').style.display = 'none';
+    document.getElementById('customAlert5').style.display = 'none';
+    document.getElementById('customAlert6').style.display = 'none';
+}
